2f4d997f1f9702418afaed0bf3caf416780104cc
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-22 15:20:01 +0600

    firebase deploy

--- src/pages/Home/Featured.jsx
+++ src/pages/Home/Featured.jsx
@@ -1,15 +1,15 @@
 import SectionTitle from "../Shared/SectionTitle";
-// import featuredImg from '../../../public/assets/home/featured.jpg'
+import featuredImg from '../../../public/assets/home/featured.jpg'
 
 const Featured = () => {
     return (
-        <div className="pt-12" style={{backgroundImage: 'url(../../../public/assets/home/featured.jpg)'}}>
+        <div className="pt-12" style={{backgroundImage: `url(${featuredImg})`}}>
             <SectionTitle
                 subHeading={"Check it out"}
                 heading={"from our menu"}
             ></SectionTitle>
             <div className="md:flex justify-center items-center gap-8 px-40 py-20">
-                <img className="w-[460px] h-[300px]" src="../../../public/assets/home/featured.jpg" alt="" />
+                <img className="w-[460px] h-[300px]" src={featuredImg} alt="" />
                 <div className="text-white">
                     <h4 className="text-lg">March 20, 2023</h4>
                     <p className="uppercase text-lg">whre can i get some?</p>
